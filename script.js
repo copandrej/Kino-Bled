c5a872010f6b6030deb7517f100f05862a680eba
copandrej <ac8560@student.uni-lj.si>
2021-09-04 21:13:16 +0200

    JSON-izacija spletne strani

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 function zapri(el) {
     (el.clientHeight >= 45) ? el.style.height = "41px" : el.style.height = (el.childNodes[3].clientHeight + 41).toString() + "px";
 }
+function poravnaj(el) {
+    if (el.clientHeight >= 45) el.style.height = (el.childNodes[3].clientHeight + 41).toString() + "px";
+}
 
 
 window.addEventListener("load", () => {
     let DneviSlo = ["TOREK", "SREDA", "ČETRTEK", "PETEK", "SOBOTA"];
     let DneviEng = ["TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
     let userLang = navigator.language || navigator.userLanguage; 
-    let trenutniJezik = "en"
+    let trenutniJezik = ""
+
     if(userLang == "sl" && trenutniJezik != "sl") 
         spremembaJezika("sl");
+    else if (userLang != "sl" && trenutniJezik != "en")
+        spremembaJezika("en");
 
-    document.querySelector(".slo").addEventListener("click", () => {spremembaJezika("sl");});
-    document.querySelector(".eng").addEventListener("click", () => {spremembaJezika("en");});
-
+    $(".slo").click(() => {spremembaJezika("sl");});
+    $(".eng").click(() => {spremembaJezika("en");});
 
     function spremembaJezika(Jezik) {
         let spremeni = document.getElementsByClassName("dan");
@@ -22,8 +27,10 @@ window.addEventListener("load", () => {
             trenutniJezik = "sl";     
             for (let i = 0; i < spremeni.length; i++)
                 spremeni[i].innerText = DneviSlo[i];
+
             prevedi[0].innerText = "ZAVOD ASPEKT    PREDSTAVLJA";
             prevedi[1].innerText = "PIŠITE NAM";
+            vsebina(trenutniJezik);
             
         } else {
             trenutniJezik = "en";
@@ -31,7 +38,27 @@ window.addEventListener("load", () => {
                 spremeni[i].innerText = DneviEng[i];
             prevedi[0].innerText = "ASPEKT INSTITUTE    PRESENTS";
             prevedi[1].innerText = "EMAIL US";
-
+            vsebina(trenutniJezik);
         }
+    }
+
+    function vsebina(jezik) {
+        let imeDatoteke = `vsebina/${jezik}.json`
+        console.log(imeDatoteke);
+        $.getJSON(imeDatoteke, (data) => {
+            let dnevi = document.getElementsByClassName("skri");
+            for (let i = 0; i < dnevi.length; i++) {  
+                let string = ""; 
+                for (let k = 0; k < data[DneviEng[i]].length; k++) {
+                    string +=  `<h3>${data[DneviEng[i]][k].naslov}</h3><p>`;
+                    for (let j = 0; j < data[DneviEng[i]][k].vrstice.length; j++) {
+                        string += `${data[DneviEng[i]][k].vrstice[j]}<br>`
+                    }
+                    string += "</p>"
+                }
+                dnevi[i].innerHTML = string;
+                poravnaj(dnevi[i].parentNode);
+            } 
+      });
     }
 });
